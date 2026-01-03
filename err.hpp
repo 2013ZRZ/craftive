6fbf919bf71b7f943282054f4b59c348d7240cd9
2013ZRZ <zrz_2013@outlook.com>
2026-01-03 20:30:34 +0800

    Add .clang-format, etc

--- err.hpp
+++ err.hpp
@@ -2,47 +2,46 @@
 
 #include <cctype>
 #include <ctime>
-#include <iomanip>
 #include <random>
-#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
 
-/*std::string hex2str(const int hex) {
-	std::stringstream ss;
-	ss << std::hex << hex;
-	return ss.str();
-}*/
-
-const std::unordered_map<unsigned short, std::string> errtable {
-	{0x0001, "0x0001 Empty ID"},
-	{0x0002, "0x0002 The ID includes invalid character(s)"},
-	{0x0003, "0x0003 Invalid color code; type `help --color-code` for more info"},
-	{0x0004, "0x0004 No such block or combination in this kit"},
-	{0x0005, "0x0005 Invalid position"},
-	{0x0006, "0x0006 Failed to parse JSON"}
-};
+const std::unordered_map<unsigned short, std::string> errtable{
+    {0x0001, "0x0001 Empty ID"},
+    {0x0002, "0x0002 The ID includes invalid character(s)"},
+    {0x0003,
+     "0x0003 Invalid color code; type `help --color-code` for more info"},
+    {0x0004, "0x0004 No such element in this kit"},
+    {0x0005, "0x0005 Invalid position"},
+    {0x0006, "0x0006 Failed to parse JSON"}};
 class errcode : public std::runtime_error {
-public:
-	errcode(const unsigned short code) : std::runtime_error(/*hex2str(code) + " " + */errtable.at(code)) {}
-	errcode(const std::string& msg) : std::runtime_error("0x0000 Extern Error: " + msg) {}
+  public:
+    errcode(const unsigned short code)
+        : std::runtime_error(errtable.at(code)) {}
+    errcode(const std::string &msg)
+        : std::runtime_error("0x0000 Extern Error: " + msg) {}
 };
 
-bool isValidID(const std::string& id) {
-	for (const auto it : id) {
-		if (!isalnum(it) && it != '_') return false;
-	}
-	return true;
+bool isValidID(const std::string &id) {
+    for (const auto it : id) {
+	if (!isalnum(it) && it != '_')
+	    return false;
+    }
+    return true;
 }
 
 std::string randomID() {
-	std::string o;
-	std::mt19937_64 r(time(nullptr));
-	for (int i = 0; i < 50; i++) {
-		unsigned r_ = r() % 26;
-		if (r_ < 25) o.push_back((char)'a' + r_);
-		else o.push_back('_');
+    std::random_device rd;
+    std::string o;
+    std::mt19937_64 r(time(nullptr) + rd());
+    for (int i = 0; i < 50; i++) {
+	unsigned r_ = r() % 26;
+	if (r_ < 25) {
+	    o.push_back((char)'a' + r_);
+	} else {
+	    o.push_back('_');
 	}
-	return o;
+    }
+    return o;
 }
