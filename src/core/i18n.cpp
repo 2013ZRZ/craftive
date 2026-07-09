ea40e0bc779b40d4d96c14054fce6c746a47b3c7
ZRZ <zrz_2013@outlook.com>
2026-07-09 18:43:58 +0800

    Add translations English and English (United States) & optimize i18nInit()

--- src/core/i18n.cpp
+++ src/core/i18n.cpp
@@ -1,62 +1,44 @@
 #include "i18n.hpp"
+#include <QtCore/QCoreApplication>
 #include <QtCore/QDir>
-#include <QtCore/QFileInfo>
-#include <QtCore/QLibraryInfo>
 #include <QtCore/QString>
 #include <QtCore/QTranslator>
+
+
+namespace {
+static QTranslator &appTranslator() {
+    static QTranslator inst;
+    return inst;
+}
+} // namespace
 
 void i18nInit() {
     auto app = QCoreApplication::instance();
     if (!app)
         return;
 
+    QString localeName = QLocale::system().name();
+
     QString translationsDir;
 #ifdef _CRAFTIVE_DEBUG
 #ifdef _CRAFTIVE_DEBUGGING_LOCALES_DIR
     translationsDir = QString::fromUtf8(_CRAFTIVE_DEBUGGING_LOCALES_DIR);
 #else
-#warning "_CRAFTIVE_DEBUGGING_LOCALES_DIR is undefined; using ./locales as the locales directory"
-    translationsDir = QDir::currentPath() + "/locales";
+#error "_CRAFTIVE_DEBUGGING_LOCALES_DIR is undefined"
 #endif
 #else
 #if defined(_WIN32) || defined(__APPLE__)
-    QString   execPath = QApplication::applicationFilePath();
-    QFileInfo fi{execPath};
-    QDir      dir = fi.absoluteDir();
-#if defined(_WIN32)
-    translationsDir = dir.absolutePath() + "/locales";
-#elif defined(__APPLE__)
-    dir.cdUp();
-    translationsDir = dir.absolutePath() + "/Resources/locales";
+    translationsDir = QDir(QApplication::applicationDirPath()).absolutePath();
+#if defined(__APPLE__)
+    translationsDir += "/Contents/Resources/locales";
+#else
+    translationsDir += "/locales";
 #endif
 #else
-    translationsDir = "/usr/share/locale";
+    translationsDir = "/usr/share/locale/" + localeName + "/LC_MESSAGES";
 #endif
 #endif
 
-    QString      localeName   = QLocale::system().name();
-    QTranslator *qtTranslator = new QTranslator{app};
-    if (qtTranslator->load("qt_" + localeName, QLibraryInfo::path(QLibraryInfo::TranslationsPath)))
-        app->installTranslator(qtTranslator);
-    else
-        delete qtTranslator;
-    QTranslator *appTranslator = new QTranslator{app};
-    if (appTranslator->load("craftive." + localeName,
-                            translationsDir
-#if !(defined(_WIN32) || defined(__APPLE__))
-                                + "/" + localeName + "/LC_MESSAGES"
-#endif
-                            ))
-        app->installTranslator(appTranslator);
-    else {
-        delete appTranslator;
-        appTranslator = new QTranslator(app);
-        if (appTranslator->load("craftive.en_US",
-                                translationsDir
-#if !(defined(_WIN32) || defined(__APPLE__))
-                                    + "/" + localeName + "/LC_MESSAGES"
-#endif
-                                ))
-            app->installTranslator(appTranslator);
-    }
+    if (appTranslator().load("craftive." + localeName, translationsDir))
+        app->installTranslator(&appTranslator());
 }
